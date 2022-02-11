8932aed41d326138e0dd7e3ef369bfc780605692
ezirmusitua <jferroal@gmail.com>
2022-02-11 01:00:14 +0000

    Fix: desktop_app: today article saved_at

--- packages/desktop_app/src/pages/Today.action.ts
+++ packages/desktop_app/src/pages/Today.action.ts
@@ -5,11 +5,18 @@ export default class TodayAction {
     constructor(private readonly ctx: iTodayContextValue) {}
 
     async init() {
-        // TODO: add init today api, create if not exist
-        const resp = await article.get_today();
+        const { data } = await article.get_today();
         this.ctx.dispatch({
             type: "set_items",
-            payload: [resp.data],
+            payload: [
+                {
+                    ...data,
+                    items: data.items.map(({ saved_at, ...i }) => ({
+                        ...i,
+                        saved_at: new Date(saved_at),
+                    })),
+                },
+            ],
         });
         this.ctx.dispatch({ type: "set_initialized", payload: true });
     }
